65fffc8e1b7ed2d7a25b3ed11595077886c745e6
Bevan Philip <bevan.philip@cumulocity.com>
2024-09-25 12:25:03 +0100

    Fix tests (wrong extension name)

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -10,11 +10,11 @@ suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
     test('Extension should be present', () => {
-        assert.ok(vscode.extensions.getExtension('apama-extensions'));
+        assert.ok(vscode.extensions.getExtension('ApamaCommunity.apama-extensions'));
     });
 
     test('Extension should activate', async () => {
-        const ext = vscode.extensions.getExtension('apama-extensions');
+        const ext = vscode.extensions.getExtension('ApamaCommunity.apama-extensions');
         await ext?.activate();
         assert.ok(ext?.isActive);
     });
