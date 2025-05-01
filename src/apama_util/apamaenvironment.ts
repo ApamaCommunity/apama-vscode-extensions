e58b5e9a9ccf4cb33bcf7087fd5bd94b3e476c86
Bevan Philip <bevan.philip@cumulocity.com>
2025-05-01 10:39:46 +0100

    feat: simplify API interface

--- src/apama_util/apamaenvironment.ts
+++ src/apama_util/apamaenvironment.ts
@@ -24,25 +24,24 @@ export interface ApamaExecutableInterface {
 }
 
 /**
- * Gets the `apama_env` command for this platform.
- * We reload this on every invocation because it should be relatively rare. 
- * @returns False if Apama can't be found, `apama_env` otherwise.
+ * Determines the correct command line & parameters to run the command.
+ * @returns False if Apama can't be found, the command & args pair to run the command otherwise
  */
-async function getApamaEnvCommand(showError=true) {
+async function getApamaExecutableCommand(command: ApamaExecutables, showError=true) {
   const apama = await determineIfApamaExists();
   if (apama != false) {
     const apamaBin = path.dirname(apama);
     if (platform() === "linux") {
       const apama_env_path = `${apamaBin}/apama_env`;
       if (existsSync(apama_env_path)) {
-        return ok({"apama_env": true, "path": apama_env_path});
+        return ok({command: apama_env_path, args: [command]})
       } else {
         // Assume we're in a Docker container without apama_env, and the commands can be
         // invoked manually.
-        return ok({"apama_env": false, "path": apamaBin})
+        return ok({command: `${apamaBin}/${command}`, args: []})
       }
     } else {
-      return ok({"apama_env": true, "path": `${apamaBin}/apama_env.bat`});
+      return ok({command: `${apamaBin}/apama_env.bat`, args: [command]});
     }
   }
 
@@ -53,26 +52,23 @@ async function getApamaEnvCommand(showError=true) {
 }
 
 export async function getCommandLine(command: ApamaExecutables, showError=true) {
-  const apama_env = await getApamaEnvCommand(showError);
-  if (apama_env.isOk()) {
-    if (apama_env.value.apama_env != false) {
-      return `${apama_env.value.path} ${command}`;
-    } else {
-      return `${apama_env.value.path}/${command}`;
-    }
-
+  const apama_executable_command = await getApamaExecutableCommand(command, showError);
+  if (apama_executable_command.isOk()) {
+    let command = `${apama_executable_command.value.command}`
+    // We do this rather than `args.join(" ")`, because then we don't introduce any 
+    // odd spaces. Also, I imagine this is optimized out at some level of the interpreter.
+    apama_executable_command.value.args.forEach((x) => command = command + x);
+    return command;
   }
   return false;
 }
 
 export async function getCommandAsInterface(command: ApamaExecutables, showError=true): Promise<false | ApamaExecutableInterface> {
-  const apama_env = await getApamaEnvCommand(showError);
-  if (apama_env.isOk()) {
-    if (apama_env.value.apama_env != false) {
-      return { command: apama_env.value.path, args: [command] };
-    } else {
-      return { command: `${apama_env.value.path}/${command}`, args: [] }
-    }
+  const apama_executable_command = await getApamaExecutableCommand(command, showError);
+
+  if (apama_executable_command.isOk()) {
+    return apama_executable_command.value;
   }
+
   return false;
 }
