81b885b7462f957647ab062a0047d1e15a06f9d3
Bevan Philip <bevan.philip@cumulocity.com>
2025-05-01 09:45:50 +0100

    feat: support apama paths without `apama_env`
    - we already only check for the correlator process to determine
    if we're have an apama instance,
    - this just now supports executing with or without apama_env in that
    directory. we assume if there is no apama_env, you're in a docker
    container that supports running those commands without apama_env
    (because the environment is already configured)

--- src/apama_util/apamaenvironment.ts
+++ src/apama_util/apamaenvironment.ts
@@ -2,6 +2,8 @@ import { platform } from "os";
 import { determineIfApamaExists } from "../extension";
 import { window } from "vscode";
 import * as path from 'path';
+import { existsSync } from "fs";
+import { ok, err } from 'neverthrow';
 
 export enum ApamaExecutables {
   CORRELATOR = "correlator",
@@ -26,35 +28,51 @@ export interface ApamaExecutableInterface {
  * We reload this on every invocation because it should be relatively rare. 
  * @returns False if Apama can't be found, `apama_env` otherwise.
  */
-async function getApamaEnvCommand(showError=true): Promise<false | string> {
+async function getApamaEnvCommand(showError=true) {
   const apama = await determineIfApamaExists();
   if (apama != false) {
     const apamaBin = path.dirname(apama);
     if (platform() === "linux") {
-      return Promise.resolve(`${apamaBin}/apama_env`);
+      const apama_env_path = `${apamaBin}/apama_env`;
+      if (existsSync(apama_env_path)) {
+        return ok({"apama_env": true, "path": apama_env_path});
+      } else {
+        // Assume we're in a Docker container without apama_env, and the commands can be
+        // invoked manually.
+        return ok({"apama_env": false, "path": apamaBin})
+      }
     } else {
-      return Promise.resolve(`${apamaBin}/apama_env.bat`);
+      return ok({"apama_env": true, "path": `${apamaBin}/apama_env.bat`});
     }
   }
 
   if (showError) {
     window.showErrorMessage(`Could not find Apama in your environment: you can configure the "Apama Home" setting to specify an install location.`);
   }
-  return Promise.resolve(false);
+  return err();
 }
 
 export async function getCommandLine(command: ApamaExecutables, showError=true) {
   const apama_env = await getApamaEnvCommand(showError);
-  if (apama_env != false) {
-    return `${apama_env} ${command}`;
-  }  
+  if (apama_env.isOk()) {
+    if (apama_env.value.apama_env != false) {
+      return `${apama_env.value.path} ${command}`;
+    } else {
+      return `${apama_env.value.path}/command`;
+    }
+
+  }
   return false;
 }
 
 export async function getCommandAsInterface(command: ApamaExecutables, showError=true): Promise<false | ApamaExecutableInterface> {
   const apama_env = await getApamaEnvCommand(showError);
-  if (apama_env != false) {
-    return { command: apama_env, args: [command] };
-  }   
+  if (apama_env.isOk()) {
+    if (apama_env.value.apama_env != false) {
+      return { command: apama_env.value.path, args: [command] };
+    } else {
+      return { command: `${apama_env.value.path}/command`, args: [] }
+    }
+  }
   return false;
 }
