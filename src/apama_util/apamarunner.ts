5f6c34dd1a78f0287b36446e6deec6e194a3c5af
Ben Spiller <84027153+ben-c8y@users.noreply.github.com>
2025-05-02 15:01:54 +0100

    add progress notification while running commands liek apama_project

--- src/apama_util/apamarunner.ts
+++ src/apama_util/apamarunner.ts
@@ -2,6 +2,10 @@ import { exec, execFile} from "child_process";
 import { platform } from "os";
 import { promisify } from "util";
 import { logger } from "../extension";
+import {
+  ProgressLocation,
+  window
+} from "vscode";
 
 const execPromisify = promisify(exec);
 const execFilePromisify = promisify(execFile);
@@ -20,29 +24,35 @@ export class ApamaRunner {
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   async run(workingDir: string, args: string[]): Promise<any> {
-    try {
-      //if fails returns promise.reject including err
-      if (platform() === "win32") {
-        logger.debug("Running command: " + this.command[0] + " " + [...this.command.slice(1), ...args].join(" "));
-        // Not ideal: exec is not so secure, but execFile doesn't work with .bat file and spawn hangs mysteriously (even with stdio ignore),
-        // so given Windows isn't a main platform going forward and that there isn't a real way this could be used to escalate privilieges, this will do for now
-        return await execPromisify([...this.command, ...args].join(" "), { cwd: workingDir });
-      } else {
-        return await execFilePromisify(
-          this.command[0],
-          [...this.command.slice(1), ...args],
-          { cwd: workingDir }
-        );
+    return await window.withProgress(
+      {
+        // since sometimes (e.g. apama_project) the command takes a long time, giving a progress notification is helpful
+        location: ProgressLocation.Notification,
+        title: `Running ${this.name}...`,
+        cancellable: false,
+      },
+      async () => 
+      {
+        try {
+          if (platform() === "win32") {
+            logger.debug("Running command: " + this.command[0] + " " + [...this.command.slice(1), ...args].join(" "));
+            return await execPromisify([...this.command, ...args].join(" "), { cwd: workingDir });
+          } else {
+            return await execFilePromisify(
+              this.command[0],
+              [...this.command.slice(1), ...args],
+              { cwd: workingDir }
+            );
+          }
+        } catch (error) {
+          const enhancedError = new Error(
+            `Error running command '${this.command[0]} ${[...this.command.slice(1), ...args].join(" ")}': ${error}`
+          );
+          if (error instanceof Error) enhancedError.stack = error.stack;
+          throw enhancedError;
+        }
       }
-    } catch (error) {
-      const enhancedError = new Error(
-        `Error running command '${this.command[0]} ${[...this.command.slice(1), ...args].join(" ")}': ${error}`
-      );
-      if (error instanceof Error) {
-        enhancedError.stack = error.stack;
-      }
-      throw enhancedError;
-    }
+    );
   }
 }
 
